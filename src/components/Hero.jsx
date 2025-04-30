d7e71caf55307169e36887895265e94746579f2f
jedsalanguit <jlyndon.salanguit@gmail.com>
2025-05-01 01:16:19 +0800

    Updated Resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,47 +1,77 @@
-import {HERO_CONTENT} from "../constants"
-import heroPic from "../assets/JedSalanguit.png"
-import resume from "../assets/JedSalanguit-Resume.pdf"
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import resume from "../assets/Jed-Salanguit-Resume.pdf";
+import heroPic from "../assets/JedSalanguit.png";
+import { HERO_CONTENT } from "../constants";
 
 const container = (delay) => ({
-    hidden: {x: -100, opacity: 0},
-    visible: {
-        x: 0,
-        opacity: 1,
-        transition: {duration: 0.5, delay: delay }
-    }
-})
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 
 const Hero = () => {
   return (
     <div className="pb-4 lg:mb-35">
-        <div className="flex flex-wrap">
-            <div className="w-full lg:w-1/2">
-                <div className="flex flex-col items-center lg:items-start">
-                    <motion.h1 variants={container(0)} initial="hidden" animate="visible" className="pb-16 text-6xl font-thin tracking-light lg:mt-16 lg:text-8xl">
-                        Jed Salanguit
-                    </motion.h1>
-                    <motion.span variants={container(0.5)} initial="hidden" animate="visible" className="bg-gradient-to-r from-purple-400 via-slate-300 to-purple-500 bg-clip-text text-3xl tracking-light text-transparent">
-                        Web Developer
-                    </motion.span>
-                    <motion.p variants={container(1)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-lighter">
-                        {HERO_CONTENT}
-                    </motion.p>
-                    <motion.button variants={container(1.3)} initial="hidden" animate="visible" className="relative h-12 w-40 overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80 mb-4"><a href={resume} download='Jed-Salanguit' className="relative z-10">Download Resume</a></motion.button>
-                </div>
-            </div>
-            <div className="w-full lg:w-1/2 lg:p-8">
-                <div className="flex justify-center">
-                    <motion.img 
-                    initial= {{x: 100, opacity: 0}}
-                    animate={{x: 0, opacity: 1}}
-                    transition={{duration:1, delay: 1.5}}
-                    src={heroPic} alt="Hero Pic" />
-                </div>
-            </div>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-light lg:mt-16 lg:text-8xl"
+            >
+              Jed Salanguit
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-purple-400 via-slate-300 to-purple-500 bg-clip-text text-3xl tracking-light text-transparent"
+            >
+              Web Developer
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-lighter"
+            >
+              {HERO_CONTENT}
+            </motion.p>
+            <motion.button
+              variants={container(1.3)}
+              initial="hidden"
+              animate="visible"
+              className="relative h-12 w-40 overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80 mb-4"
+            >
+              <a
+                href={resume}
+                download="Jed-Salanguit"
+                className="relative z-10"
+              >
+                Download Resume
+              </a>
+            </motion.button>
+          </div>
         </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
+              src={heroPic}
+              alt="Hero Pic"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
